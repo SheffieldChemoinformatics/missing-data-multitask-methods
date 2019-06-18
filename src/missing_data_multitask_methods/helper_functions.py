1f39bdb38954c8006842ff7bd7c1ff51c09a8216
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-06-18 11:24:13 +0100

    Better handling of missing data_transform argument

--- src/missing_data_multitask_methods/helper_functions.py
+++ src/missing_data_multitask_methods/helper_functions.py
@@ -266,16 +266,19 @@ def transform(train_data, test_data, args):
     """
     train_descriptors, train_labels=train_data
     test_descriptors, test_labels=test_data
-    
-    if args['data_transform'] not in [None, 'log', 'binary', 'none', 'None']:
+
+    data_transform = args.get('data_transform')
+
+    if data_transform not in [None, 'log', 'binary', 'none', 'None']:
         print('Data transformation not understood: {0}. Exiting'.format(args['data_transform']))
         sys.exit(1)
-    if args['data_transform'] is None:
+
+    if data_transform is None:
         pass
-    elif args['data_transform'].lower()=='log':
+    elif data_transform.lower() == 'log':
         train_descriptors=np.log(train_descriptors+1)
         test_descriptors=np.log(test_descriptors+1)
-    elif args['data_transform'].lower()=='binary':
+    elif data_transform.lower() == 'binary':
         train_descriptors=np.where(train_descriptors>=1, 1, 0)
         test_descriptors=np.where(test_descriptors>=1, 1, 0)
     
